d366010d3dd0693b47cf3f6718bdc0fc0d3d71b0
András D. Tóth <andras.toth@transferwise.com>
2019-04-17 15:28:18 +0200

    add title to topic page

--- pages/topic.js
+++ pages/topic.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import Link from 'next/link'
-import { getSidebarTopics, getTopicsById } from "../clients/clients";
-import { PageTemplate } from "../components/template";
+import Link from 'next/link';
+import { getSidebarTopics, getTopicsById } from '../clients/clients';
+import { PageTemplate } from '../components/template';
 
 export default class extends React.Component {
   static async getInitialProps({ query: { topicId: topicId } }) {
     const [topicResult, allTopics] = await Promise.all([getTopicsById(topicId), getSidebarTopics()]);
-
-    console.log(topicResult)
 
     return { id: topicId, topicResult, allTopics };
   }
 
   render() {
     const { topicResult, allTopics } = this.props;
+    const { title, articles } = topicResult;
 
     return (
       <PageTemplate allTopics={allTopics}>
-          <ul>
-            {topicResult.articles.map(a => (
-                <li key={a.id}>
-                  <Link key={a.id} href={`/topic/${a.parentSlug}/article/${a.id}/${a.slug}`}
-                        prefetch><a>{a.title}</a></Link>
-                </li>
-              )
-            )}
-          </ul>
+        <h1>{title}</h1>
+        <ul>
+          {articles.map(a => (
+              <li key={a.id}>
+                <Link key={a.id} href={`/topic/${a.parentSlug}/article/${a.id}/${a.slug}`}
+                      prefetch><a>{a.title}</a></Link>
+              </li>
+            )
+          )}
+        </ul>
       </PageTemplate>
-    )
+    );
   }
 }
